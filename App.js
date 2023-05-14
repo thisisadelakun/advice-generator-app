b5ac0b99da1c1e7c90a7d560eff5811b5b8202d8
Adelakun Abayomi <adelakunabayomi123@gmail.com>
2023-05-14 10:36:13 +0100

    lowercase commit

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-let Number = document.getElementById("advice-number");
-let Advice = document.getElementById("advice-text");
+let num = document.getElementById("advice-number");
+let adv = document.getElementById("advice-text");
 let btn = document.getElementById("btn");
 
 function getAdvice() {
     fetch('https://api.adviceslip.com/advice')
         .then((res) => res.json())
         .then((data) => {
-            Number.innerText = `ADVICE #${data.slip.id}`;
-            Advice.innerText = `"${data.slip.advice}"`;
+            num.innerText = `ADVICE #${data.slip.id}`;
+            adv.innerText = `"${data.slip.advice}"`;
         })
         .catch((error) => {
             console.error('error:', error);
